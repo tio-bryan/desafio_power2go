b37d652d90fdf0b0abad6175f7605534c0b5f2e0
Bryan Munekata <bryan.pesca@gmail.com>
2024-05-25 17:01:16 -0300

    Starting repo

---
+++ example.py
@@ -0,0 +1,9 @@
+''' Descricao de Modulo '''
+
+def minha_funcao():
+    ''' Descricao da funcao '''
+    print('Ola')
+
+def essa_e_minha_funcao():
+    ''' Descricao da funcao '''
+    print('Ola')
